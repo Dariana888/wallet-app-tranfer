47519300c5d9be157f5a312507445b7595c06055
Sorin Gore <soringoredev@gmail.com>
2026-03-09 22:35:21 +0200

    Add ex2

--- python_list_sorin.py
+++ python_list_sorin.py
@@ -3,3 +3,9 @@ lista_cumparaturi = ['paine', 'lapte', 'covrigi']
 print(f'Lista originala:  {lista_cumparaturi}')
 lista_cumparaturi[0] = 'paine cu toast'
 print(f'Lista modificata: {lista_cumparaturi}')
+
+# adaugam doua produse noi
+lista_cumparaturi.append('oua')
+lista_cumparaturi.append('salam')
+
+print(lista_cumparaturi)
