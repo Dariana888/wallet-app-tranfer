0716277f69c066bd7aef5a57e58ec514c09c7123
Sorin Gore <soringoredev@gmail.com>
2026-03-09 22:26:26 +0200

    Add folder script

---
+++ folder_info_sorin.py
@@ -0,0 +1,31 @@
+# import os
+#
+# folder = 'C:/Users/sorin/Desktop/raport'
+#
+# files = 0
+# folders = 0
+# total_size = 0
+#
+# for item in os.listdir(folder):
+#     path = os.path.join(folder, item)
+#
+#     if os.path.isfile(path):
+#         files += 1
+#         total_size += os.path.getsize(path)
+#     elif os.path.isdir(path):
+#         folders += 1
+#
+# print('Fisiere: ', files)
+# print('Foldere: ', folders)
+# print('Dimensiune: ', total_size, ' bytes')
+#
+# a = 5
+# b = 10
+# c = 56
+#
+# numere = [1,2,3]
+
+# txt = 'zina'
+# print(len(txt))
+# print(txt[0])
+# print(txt[-4])
