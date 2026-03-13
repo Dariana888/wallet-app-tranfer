6174d893f0bcdcac31b372981656ce700950a9ac
Sorin Gore <soringoredev@gmail.com>
2026-03-13 21:22:11 +0200

    abc

--- python_list_sorin.py
+++ python_list_sorin.py
@@ -13,3 +13,15 @@ print(lista_cumparaturi)
 # printarea elementelor listei cu ajutorul buclei for
 for cumparatura in lista_cumparaturi:
     print(cumparatura)
+
+
+##################################
+################################
+lista_exemplu = ['paine', 'lapte', 'covrigi','paine', 'lapte', 'covrigi','paine', 'lapte', 'covrigi','paine', 'lapte', 'covrigi''paine', 'lapte', 'covrigi''paine', 'lapte', 'covrigi''paine', 'lapte', 'covrigi''paine', 'lapte', 'covrigi''paine', 'lapte', 'covrigi','paine', 'lapte', 'covrigi','paine', 'lapte', 'covrigi']
+
+lista_doi = ['test1', 'test2']
+print(lista_exemplu[-1])
+
+print(lista_exemplu.extend(lista_doi))
+
+print(lista_exemplu)
