33b1b51d7edc5b07854f3b234887d6321ea68db1
Sorin Gore <soringoredev@gmail.com>
2026-03-13 21:36:21 +0200

    Add exercise

---
+++ creare_lista_sorin.py
@@ -0,0 +1,11 @@
+list_name = [10, 20, 30, 40, 50]
+print(list_name)
+list_name.append(60)
+list_name.append(70)
+
+list_name.pop(0)
+list_name.pop(3)
+print(list_name)
+list_name.remove(20)
+print(list_name)
+
