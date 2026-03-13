02195af9ef9c0574dfc14745052a266985696254
Dariana888 <gontadariana2000@gmail.com>
2026-03-13 22:16:34 +0200

    Game

---
+++ Dariana/game.py
@@ -0,0 +1,45 @@
+import random
+
+game_images = [
+    """
+    _______
+---'   _____)
+      (______)
+      (______)
+      (_____)
+---.__(____)
+""",
+    """
+     _______
+---'    ____)____
+           __________)
+          __________)
+         _________)
+---.__________)
+""",
+    """
+    _______
+---'   ____)____
+          _______)
+       ____________)
+      (____)
+---.__(___)
+"""
+]
+
+computer_choice = random.randint(0, 2)
+
+user_choice = int(input('Alege: 0 = Rock, 1 = Paper, 2 = Scissors: '))
+
+print('Calculator:', game_images[computer_choice])
+print('Utilizator:', game_images[user_choice])
+if user_choice == computer_choice:
+    print('Egalitate!')
+elif user_choice == 0 and computer_choice == 2:
+    print('Ai castigat!')
+elif user_choice == 2 and computer_choice == 1:
+    print('Ai castigat!')
+elif user_choice == 1 and computer_choice == 0:
+    print('Ai castigat!')
+else:
+    print('Ai pierdut!')
