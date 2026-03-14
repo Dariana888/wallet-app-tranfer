cb32414a53a28b9496f310b9c49a4c59f17f79cc
Dariana888 <gontadariana2000@gmail.com>
2026-03-14 07:51:45 +0200

    Dictionay

---
+++ dict_dariana/dict_2.py
@@ -0,0 +1,16 @@
+# student = {
+#     'name': 'Ana',
+#     'grades': [8,9,10,7]
+# }
+#
+# print(student['grades'])
+
+
+cities_by_country = {
+    'France'  : ['Paris', 'Strasbourg', 'Dijon'],
+    'Germany' : ['Stuttgart', 'Berlin']
+}
+
+print(cities_by_country['France'])
+# Daca dorim sa afisam doar orasul Dijon
+print(cities_by_country['France'] [2])
--- dict_dariana/marks.py
+++ dict_dariana/marks.py
@@ -1,16 +1,37 @@
 marks = {
     'Dumitru': 10,
-    'Ion': 8,
-    'Maria': 7
+    'Ion'    : 8,
+    'Maria'  : 7
 }
 
+marks['Ion']   = 10
+marks['Maria'] = 10
+marks['Sorin'] = 10
+
 print(marks)
-print(type(marks))
-# Accesam elementele din dictionar
-print(marks['Dumitru'])
-print(marks['Maria'])
-# Adaugam un element in dictionar
-marks['Andre'] = 9
+
+# Verificarea existentei unei kei in dictionar
+if 'Ion' in marks:
+    print('Cheia exista.')
+else:
+    print('Cheia nu exista.')
+
+
+# Utilizez variabila de aceea am scris mark daca nu o utilizam puneam _
+# for mark in marks:
+#     print(mark)
+#     print(marks[mark])
+
+for key, value in marks.items():
+    print(key,value)
+
+# print(marks)
+# print(type(marks))
+# # Accesam elementele din dictionar
+# print(marks['Dumitru'])
+# print(marks['Maria'])
+# # Adaugam un element in dictionar
+# marks['Andre'] = 9
 
 
 user = {
@@ -21,5 +42,4 @@ user = {
 user['email'] = 'alex@email.com'
 
 print(user)
-
 
